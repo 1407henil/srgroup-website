32088f93404df74f30f75df74c42f5d23f70e1ec
1407henil <1407henil@gmail.com>
2026-07-21 15:05:29 +0530

    Create navigation.ts

---
+++ src/data/navigation.ts
@@ -0,0 +1,27 @@
+export interface NavigationItem {
+  label: string;
+  href: string;
+}
+
+export const navigation: NavigationItem[] = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "About",
+    href: "/about",
+  },
+  {
+    label: "Companies",
+    href: "/companies",
+  },
+  {
+    label: "Products",
+    href: "/products",
+  },
+  {
+    label: "Contact",
+    href: "/contact",
+  },
+];

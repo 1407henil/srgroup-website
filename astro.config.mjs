2b8209a0d1ebe250c5f81563de71698007967676
1407henil <1407henil@gmail.com>
2026-07-21 15:59:14 +0530

    Update astro.config.mjs
    chore: configure astro for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://srgroup.co.in",
-  output: "static"
+  site: "https://1407henil.github.io",
+  base: "/srgroup-website/",
+  output: "static",
 });

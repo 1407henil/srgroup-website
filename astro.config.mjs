c7492deb0bb5f10d340edacc9081e7f16704b2d2
1407henil <1407henil@gmail.com>
2026-07-23 14:36:49 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,28 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+
   site: "https://1407henil.github.io",
-  base: "/srgroup-website/",
-  output: "static",
+
+  base: "/srgroup-website",
+
+  trailingSlash: "never",
+
+  compressHTML: true,
+
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: "viewport"
+  },
+
+  build: {
+    inlineStylesheets: "auto"
+  },
+
+  vite: {
+    build: {
+      cssCodeSplit: true
+    }
+  }
+
 });
